e282133fd3debe935d95316126c5c2c51431b47d
conorn <cnolan@tcd.ie>
2018-06-15 14:16:17 +0100

    mod code

--- myAppHello/Program.cs
+++ myAppHello/Program.cs
@@ -6,7 +6,7 @@ namespace myAppHello
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World, it's been fun so far!");
         }
     }
 }
